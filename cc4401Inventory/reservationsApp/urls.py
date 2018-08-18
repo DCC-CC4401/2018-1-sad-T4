cdf01263aa607145030f1b4dff71a6e396fdb172
T Ismael Verdugo <tomas.verdugo@ug.uchile.cl>
2018-08-18 18:05:43 -0300

    modify states of loans in adminpage
    requisito nro. 57
    El sistema debe permitir que el administrador marque una o más préstamos, pudiendo cambiar su estado a recibido o perdido.

--- cc4401Inventory/reservationsApp/urls.py
+++ cc4401Inventory/reservationsApp/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('delete/', views.delete, name='delete_reservation'),
-    path('modify/', views.modify_reservations, name="modify_reservations"),
+    path('modify-spaceReservations/', views.modify_spaceReservations, name="modify_spaceReservations"),
+    path('modify-ArticleReservations/', views.modify_articleReservations, name="modify_articleReservations"),
     path('show/<int:reservation_id>', views.show, name='show_reservation'),
 ]
--- cc4401Inventory/reservationsApp/views.py
+++ cc4401Inventory/reservationsApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from spaceReservationsApp.models import SpaceReservation
+from articleReservationsApp.models import ArticleReservation
 from django.contrib import messages
 
 
@@ -29,21 +30,41 @@ def delete(request):
         return redirect('user_data', user_id=request.user.id)
 
 
-def modify_reservations(request):
+def modify_articleReservations(request):
     user = request.user
     if not (user.is_superuser and user.is_staff):
         return redirect('/')
     if request.method == "POST":
+        try:
+            reservations = ArticleReservation.objects.filter(id__in=request.POST.getlist("selected"))
+            #recibido
+            if request.POST["accept"] == "1":
+                for reservation in reservations:
+                    reservation.state = 'E'
+                    reservation.article.state = 'D'
+                    reservation.article.save()
+                    reservation.save()
+            #perdido
+            else:
+                for reservation in reservations:
+                    reservation.state = 'L'
+                    reservation.article.state = 'L'
+                    reservation.article.save()
+                    reservation.save()
+        except:
+            messages.warning(request, 'Error: el cambio de estado no se ha efectuado.')
+    return redirect("/admin/actions-panel/")
 
-        accept = True if (request.POST["accept"] == "1") else False
+
+def modify_spaceReservations(request):
+    user = request.user
+    if not (user.is_superuser and user.is_staff):
+        return redirect('/')
+    if request.method == "POST":
         reservations = SpaceReservation.objects.filter(id__in=request.POST.getlist("selected"))
-        if accept:
-            for reservation in reservations:
-                reservation.state = 'A'
-                reservation.save()
-        else:
-            for reservation in reservations:
-                reservation.state = 'R'
-                reservation.save()
-
+        #A de aceptado, R de rechazado
+        new_state = 'A' if (request.POST["accept"] == "1") else 'R'
+        for reservation in reservations:
+            reservation.state = new_state
+            reservation.save()
     return redirect('/admin/actions-panel')
