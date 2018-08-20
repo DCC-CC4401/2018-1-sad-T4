982cba29c9d418675cabb9b9838ee5babbe325ec
nevz <sreisenegger@gmail.com>
2018-08-20 18:14:32 -0300

    agregar template y fix url

--- cc4401Inventory/spacesApp/urls.py
+++ cc4401Inventory/spacesApp/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('delete/<int:space_id>', views.space_delete, name='space_delete'),
+    path('<int:space_id>', views.space_data, name='space_data'),
+    path('<int:space_id>/edit_name', views.space_edit_name, name='space_edit_name'),
+    path('<int:space_id>/edit_image', views.space_edit_image, name='space_edit_image'),
+    path('<int:space_id>/edit_description', views.space_edit_description, name='space_edit_description'),
 ]
