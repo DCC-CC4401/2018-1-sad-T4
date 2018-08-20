efcd5d6ec0f613e82dfee12562d1b10a860a5f95
José Cañete <joserodolfocl@gmail.com>
2018-08-20 18:14:15 -0300

    merge todo

--- cc4401Inventory/cc4401Inventory/urls.py
+++ cc4401Inventory/cc4401Inventory/urls.py
@@ -27,5 +27,6 @@ urlpatterns = [
     path('article/', include('articlesApp.urls')),
     path('reservation/', include('reservationsApp.urls')),
     path('space/', include('spacesApp.urls')),
-
+    path('loan/', include('loansApp.urls')),
+    
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- cc4401Inventory/loansApp/urls.py
+++ cc4401Inventory/loansApp/urls.py
@@ -1,1 +1,7 @@
 from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('show/space/<int:reservation_id>', views.show_space_loan, name='show_space_loan'),
+    path('show/article/<int:reservation_id>', views.show_article_loan, name='show_article_loan'),
+]
